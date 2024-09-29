27027a0a919d42d925a294ee7d05592dcd427beb
Cha Juyeon <ch4102109@gmail.com>
2024-09-29 16:18:53 +0900

    0929_noticeDetail

--- src/main/java/com/artineer/artineer_renewal/entity/Gallery.java
+++ src/main/java/com/artineer/artineer_renewal/entity/Gallery.java
@@ -4,10 +4,12 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.ErrorResponse;
 
 @Entity
 @Getter
+@Setter
 @Table(name = "gallery")
 @NoArgsConstructor
 @AllArgsConstructor
