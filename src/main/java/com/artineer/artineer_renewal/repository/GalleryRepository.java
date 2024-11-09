e9015bd4d29684ba9c73ccb13fc1b431a6912634
junho <jangjunho0103@naver.com>
2024-11-09 15:49:11 +0900

    mypage_activity1 edit

--- src/main/java/com/artineer/artineer_renewal/repository/GalleryRepository.java
+++ src/main/java/com/artineer/artineer_renewal/repository/GalleryRepository.java
@@ -1,6 +1,7 @@
 package com.artineer.artineer_renewal.repository;
 
 import com.artineer.artineer_renewal.entity.Gallery;
+import com.artineer.artineer_renewal.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,4 +10,5 @@ import java.util.List;
 public interface GalleryRepository extends JpaRepository<Gallery, Long> {
     @Query("select m from Gallery m order by m.no desc")
     List<Gallery> findAllGallery();
+    List<Gallery> findAllByUser(User user);
 }
