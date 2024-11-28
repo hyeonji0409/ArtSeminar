3be458af645762508454ab2137012dbfb6ed4c0a
githings <b22042@naver.com>
2024-11-28 19:57:44 +0900

    수정! 공지사항 css, 검색 복구
    1. 테이블 내 여백 줄임
    2. 검색 기능 정상화

--- src/main/java/com/artineer/artineer_renewal/controller/NoticeController.java
+++ src/main/java/com/artineer/artineer_renewal/controller/NoticeController.java
@@ -56,6 +56,8 @@ public class NoticeController {
         String username = authentication.getName();
         User user = userRepository.findByUsername(username);
 
+        System.out.println("받아온 값" + queryType +"~" + query);
+
         Pageable pageable = PageRequest.of(
                 page - 1,
                 pageSize,
